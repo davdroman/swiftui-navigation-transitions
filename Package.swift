7aaf7f1cf558eddb9661fbf6da47b8761e72c3f1
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-06 18:44:23 +0000

    wip

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
 
 package.dependencies = [
     .package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "0.1.4"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.6.0"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.6.0"), // dev
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.5.0"),
 ]
 
