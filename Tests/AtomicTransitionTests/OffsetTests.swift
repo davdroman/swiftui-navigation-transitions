76ff2a542c46e7ea9e16ffc45017ca41353156e1
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-06 17:55:20 +0000

    wip

---
+++ Tests/AtomicTransitionTests/OffsetTests.swift
@@ -0,0 +1,47 @@
+@_spi(package) @testable import Animator
+@_spi(package) import AtomicTransition
+import CustomDump
+import XCTest
+
+final class OffsetTests: XCTestCase {
+    let animatorUsed = UnimplementedAnimator()
+    let uiViewUsed = UIView()
+    lazy var viewUsed = AnimatorTransientView(uiViewUsed)
+    let properties = AnimatorTransientViewProperties(alpha: 1, transform: .identity)
+    let containerUsed = UIView()
+    lazy var contextUsed = MockedContext(containerView: containerUsed)
+
+    func testInsertion() {
+        AtomicTransition.offset(x: 100, y: 200).prepare(animatorUsed, or: viewUsed, for: .insertion, in: contextUsed)
+
+        var initial = properties
+        initial.transform = .identity.translatedBy(x: 100, y: 200)
+        XCTAssertNoDifference(viewUsed.initial, initial)
+
+        var animation = properties
+        animation.transform = .identity.translatedBy(x: 0, y: 0)
+        XCTAssertNoDifference(viewUsed.animation, animation)
+
+        let completion = properties
+        XCTAssertNoDifference(viewUsed.completion, completion)
+    }
+
+    func testRemoval() {
+        AtomicTransition.offset(x: 100, y: 200).prepare(animatorUsed, or: viewUsed, for: .removal, in: contextUsed)
+
+        let initial = properties
+        XCTAssertNoDifference(viewUsed.initial, initial)
+
+        var animation = properties
+        animation.transform = .identity.translatedBy(x: 100, y: 200)
+        XCTAssertNoDifference(viewUsed.animation, animation)
+
+        var completion = properties
+        completion.transform = .identity.translatedBy(x: 0, y: 0)
+        XCTAssertNoDifference(viewUsed.completion, completion)
+    }
+
+    // TODO: assert for (x, y | x | y) conveniences equality when this is done:
+    // https://github.com/davdroman/swiftui-navigation-transitions/discussions/6
+    // func testConveniences() {}
+}
