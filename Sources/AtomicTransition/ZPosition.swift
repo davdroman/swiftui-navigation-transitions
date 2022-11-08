a94cbeae90d7e41cfa9fb39c2e4d8e77484172db
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-08 17:51:59 +0000

    wip

--- Sources/AtomicTransition/ZPosition.swift
+++ Sources/AtomicTransition/ZPosition.swift
@@ -2,30 +2,22 @@
 import class UIKit.UIView
 
 /// A transition that brings the view to the front, regardless of insertion or removal.
-public struct BringToFront: MirrorableAtomicTransition {
+public struct BringToFront: AtomicTransition {
     public init() {}
 
     public func transition(_ view: TransientView, for operation: TransitionOperation, in container: Container) {
         container.bringSubviewToFront(view.uiView)
-    }
-
-    public func mirrored() -> SendToBack {
-        SendToBack()
     }
 }
 
 extension BringToFront: Hashable {}
 
 /// A transition that sends the view to the back, regardless of insertion or removal.
-public struct SendToBack: MirrorableAtomicTransition {
+public struct SendToBack: AtomicTransition {
     public init() {}
 
     public func transition(_ view: TransientView, for operation: TransitionOperation, in container: Container) {
         container.sendSubviewToBack(view.uiView)
-    }
-
-    public func mirrored() -> some AtomicTransition {
-        BringToFront()
     }
 }
 
