2df8cb1d9bd8c4e6f637f60348941ecd6a47d3e9
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-08 20:00:46 +0000

    wip

--- Sources/AtomicTransition/Asymmetric.swift
+++ Sources/AtomicTransition/Asymmetric.swift
@@ -1,41 +1,5 @@
 import class UIKit.UIView
 
-/// A composite transition that uses a different transition for insertion versus removal.
-public struct Asymmetric<InsertionTransition: AtomicTransition, RemovalTransition: AtomicTransition>: AtomicTransition {
-    private let insertion: InsertionTransition
-    private let removal: RemovalTransition
-
-    init(insertion: InsertionTransition, removal: RemovalTransition) {
-        self.insertion = insertion
-        self.removal = removal
-    }
-
-    public init(
-        @AtomicTransitionBuilder insertion: () -> InsertionTransition,
-        @AtomicTransitionBuilder removal: () -> RemovalTransition
-    ) {
-        self.init(insertion: insertion(), removal: removal())
-    }
-
-    public func transition(_ view: TransientView, for operation: TransitionOperation, in container: Container) {
-        switch operation {
-        case .insertion:
-            insertion.transition(view, for: operation, in: container)
-        case .removal:
-            removal.transition(view, for: operation, in: container)
-        }
-    }
-}
-
-extension Asymmetric: MirrorableAtomicTransition where InsertionTransition: MirrorableAtomicTransition, RemovalTransition: MirrorableAtomicTransition {
-    public func mirrored() -> Asymmetric<InsertionTransition.Mirrored, RemovalTransition.Mirrored> {
-        return .init(insertion: insertion.mirrored(), removal: removal.mirrored())
-    }
-}
-
-extension Asymmetric: Equatable where InsertionTransition: Equatable, RemovalTransition: Equatable {}
-extension Asymmetric: Hashable where InsertionTransition: Hashable, RemovalTransition: Hashable {}
-
 /// A transition that executes only on insertion.
 public struct OnInsertion<Transition: AtomicTransition>: AtomicTransition {
     private let transition: Transition
