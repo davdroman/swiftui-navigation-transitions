1f0a42b17b734f7756bf546cb2852d7aabbc9b8a
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-08 03:34:37 +0000

    wip

---
+++ Sources/NavigationTransition/AnyNavigationTransition.swift
@@ -0,0 +1,43 @@
+import Animation
+import UIKit
+
+public struct AnyNavigationTransition {
+    @_spi(package)public typealias Handler = (
+        AnimatorTransientView,
+        AnimatorTransientView,
+        NavigationTransitionOperation,
+        UIView
+    ) -> Void
+
+    @_spi(package)public typealias PrimitiveHandler = (
+        Animator,
+        NavigationTransitionOperation,
+        UIViewControllerContextTransitioning
+    ) -> Void
+
+    @_spi(package)public let type: Any.Type
+    @_spi(package)public let handler: Handler?
+    @_spi(package)public let primitiveHandler: PrimitiveHandler?
+    @_spi(package)public var animation: Animation = .default
+
+    public init<T: NavigationTransitionProtocol>(_ transition: T) {
+        self.type = Swift.type(of: transition)
+        self.handler = transition.transition(from:to:for:in:)
+        self.primitiveHandler = nil
+    }
+
+    public init<T: PrimitiveNavigationTransition>(_ transition: T) {
+        self.type = Swift.type(of: transition)
+        self.handler = nil
+        self.primitiveHandler = transition.transition(with:for:in:)
+    }
+}
+
+extension AnyNavigationTransition {
+    /// Attaches an animation to this transition.
+    public func animation(_ animation: Animation) -> Self {
+        var copy = self
+        copy.animation = animation
+        return copy
+    }
+}
--- Sources/NavigationTransition/NavigationTransition.swift
+++ Sources/NavigationTransition/NavigationTransition.swift
@@ -1,67 +1,4 @@
-import Animation
-@_spi(package) import Animator
 import UIKit
-
-public typealias _Animator = Animator
-
-public struct AnyNavigationTransition {
-    @_spi(package)public typealias Handler = (
-        AnimatorTransientView,
-        AnimatorTransientView,
-        NavigationTransitionOperation,
-        UIView
-    ) -> Void
-
-    @_spi(package)public typealias PrimitiveHandler = (
-        Animator,
-        NavigationTransitionOperation,
-        UIViewControllerContextTransitioning
-    ) -> Void
-
-    @_spi(package)public let type: Any.Type
-    @_spi(package)public let handler: Handler?
-    @_spi(package)public let primitiveHandler: PrimitiveHandler?
-    @_spi(package)public var animation: Animation = .default
-
-    public init<T: NavigationTransitionProtocol>(_ transition: T) {
-        self.type = Swift.type(of: transition)
-        self.handler = transition.transition(from:to:for:in:)
-        self.primitiveHandler = nil
-    }
-
-    public init<T: PrimitiveNavigationTransition>(_ transition: T) {
-        self.type = Swift.type(of: transition)
-        self.handler = nil
-        self.primitiveHandler = transition.transition(with:for:in:)
-    }
-}
-
-extension AnyNavigationTransition {
-    /// Attaches an animation to this transition.
-    public func animation(_ animation: Animation) -> Self {
-        var copy = self
-        copy.animation = animation
-        return copy
-    }
-}
-
-public enum NavigationTransitionOperation: Hashable {
-    case push
-    case pop
-
-    @_spi(package)public init?(_ operation: UINavigationController.Operation) {
-        switch operation {
-        case .push:
-            self = .push
-        case .pop:
-            self = .pop
-        case .none:
-            return nil
-        @unknown default:
-            return nil
-        }
-    }
-}
 
 /// Defines a transition which applies to two views: an origin ("from") view and a destination ("to") view.
 ///
@@ -145,5 +82,23 @@ extension NavigationTransitionProtocol where Body == Never {
             Do not access a transition's 'body' property directly, as it may not exist.
             """
         )
+    }
+}
+
+public enum NavigationTransitionOperation: Hashable {
+    case push
+    case pop
+
+    @_spi(package)public init?(_ operation: UINavigationController.Operation) {
+        switch operation {
+        case .push:
+            self = .push
+        case .pop:
+            self = .pop
+        case .none:
+            return nil
+        @unknown default:
+            return nil
+        }
     }
 }
