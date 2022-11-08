1a220b2ecc9130f1e77d4b027bbd833574e0ef1c
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-08 03:00:21 +0000

    wip [skip ci]

--- Demo/Demo/Swing.swift
+++ Demo/Demo/Swing.swift
@@ -4,22 +4,6 @@ import SwiftUI
 extension AnyNavigationTransition {
     static var swing: Self {
         .init(Swing())
-//        let angle = Angle(degrees: 70)
-//        let offset: CGFloat = 150
-//        let scale: CGFloat = 0.5
-//
-//        return .move(axis: .horizontal).combined(
-//            with: .asymmetric(
-//                push: .asymmetric(
-//                    insertion: [.rotate(-angle), .offset(x: offset), .opacity, .scale(scale)].combined(),
-//                    removal: [.rotate(angle), .offset(x: -offset)].combined()
-//                ),
-//                pop: .asymmetric(
-//                    insertion: [.rotate(angle), .offset(x: -offset), .opacity, .scale(scale), .bringToFront].combined(),
-//                    removal: [.rotate(-angle), .offset(x: offset)].combined()
-//                )
-//            )
-//        )
     }
 }
 
