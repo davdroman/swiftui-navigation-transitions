0ac3c1afcb1fe1d017a5b966eb66ac6151853b5d
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-09 01:07:05 +0000

    `Pick` Transition (#27)

---
+++ Sources/NavigationTransition/Pick.swift
@@ -0,0 +1,51 @@
+/// Used to isolate the push portion of a full `NavigationTransition` and execute it on push, ignoring the pop portion.
+public struct PickPush<Transition: NavigationTransition>: NavigationTransition {
+    private let transition: Transition
+
+    public init(@NavigationTransitionBuilder transition: () -> Transition) {
+        self.transition = transition()
+    }
+
+    public func transition(
+        from fromView: TransientView,
+        to toView: TransientView,
+        for operation: TransitionOperation,
+        in container: Container
+    ) {
+        switch operation {
+        case .push:
+            transition.transition(from: fromView, to: toView, for: operation, in: container)
+        case .pop:
+            return
+        }
+    }
+}
+
+extension PickPush: Equatable where Transition: Equatable {}
+extension PickPush: Hashable where Transition: Hashable {}
+
+/// Used to isolate the pop portion of a full `NavigationTransition` and execute it on pop, ignoring the push portion.
+public struct PickPop<Transition: NavigationTransition>: NavigationTransition {
+    private let transition: Transition
+
+    public init(@NavigationTransitionBuilder transition: () -> Transition) {
+        self.transition = transition()
+    }
+
+    public func transition(
+        from fromView: TransientView,
+        to toView: TransientView,
+        for operation: TransitionOperation,
+        in container: Container
+    ) {
+        switch operation {
+        case .push:
+            return
+        case .pop:
+            transition.transition(from: fromView, to: toView, for: operation, in: container)
+        }
+    }
+}
+
+extension PickPop: Equatable where Transition: Equatable {}
+extension PickPop: Hashable where Transition: Hashable {}
--- Tests/AtomicTransitionTests/OnTests.swift
+++ Tests/AtomicTransitionTests/OnTests.swift
@@ -1,28 +1,6 @@
 @_spi(package) import AtomicTransition
 import TestUtils
 
-final class AsymmetricTests: XCTestCase {
-    func testInsertion() {
-        let expectation = expectation(description: "Handler called")
-        let sut = Asymmetric(
-            insertion: { Spy { expectation.fulfill() } },
-            removal: { Spy { XCTFail() } }
-        )
-        sut.transition(.unimplemented, for: .insertion, in: .unimplemented)
-        wait(for: [expectation], timeout: 0)
-    }
-
-    func testRemoval() {
-        let expectation = expectation(description: "Handler called")
-        let sut = Asymmetric(
-            insertion: { Spy { XCTFail() } },
-            removal: { Spy { expectation.fulfill() } }
-        )
-        sut.transition(.unimplemented, for: .removal, in: .unimplemented)
-        wait(for: [expectation], timeout: 0)
-    }
-}
-
 final class OnInsertionTests: XCTestCase {
     func testInsertion() {
         let expectation = expectation(description: "Handler called")
