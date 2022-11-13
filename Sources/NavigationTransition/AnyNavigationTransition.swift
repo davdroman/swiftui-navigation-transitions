4581b45abf5f604e8530f65fab636cfe2bc82a44
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-13 12:46:09 +0000

    wip

--- Sources/NavigationTransition/AnyNavigationTransition.swift
+++ Sources/NavigationTransition/AnyNavigationTransition.swift
@@ -25,12 +25,12 @@ public struct AnyNavigationTransition {
     @_spi(package)public var animation: Animation = .default
 
     public init<T: NavigationTransition>(_ transition: T) {
-        self.isDefault = transition is Default
+        self.isDefault = false
         self.handler = .transient(transition.transition(from:to:for:in:))
     }
 
     public init<T: PrimitiveNavigationTransition>(_ transition: T) {
-        self.isDefault = false
+        self.isDefault = transition is Default
         self.handler = .primitive(transition.transition(with:for:in:))
     }
 }
