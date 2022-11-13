5eb07fe8a00d93d90b08d86b748ea45f93fdc0ed
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-13 12:20:17 +0000

    wip

--- Tests/AtomicTransitionTests/ZPositionTests.swift
+++ Tests/AtomicTransitionTests/ZPositionTests.swift
@@ -3,6 +3,48 @@
 import TestUtils
 
 final class ZPositionTests: XCTestCase {
+    let viewUsed = AnimatorTransientView(UIView())
+    let properties = AnimatorTransientViewProperties(
+        alpha: 1,
+        transform: .identity,
+        layer: .init(
+            zPosition: 0
+        )
+    )
+    let containerUsed = UIView()
+
+    func testInsertion() {
+        ZPosition(2).transition(viewUsed, for: .insertion, in: containerUsed)
+
+        let initial = properties
+        XCTAssertNoDifference(viewUsed.initial, initial)
+
+        var animation = properties
+        animation.layer.zPosition = 2
+        XCTAssertNoDifference(viewUsed.animation, animation)
+
+        var completion = properties
+        completion.layer.zPosition = 0
+        XCTAssertNoDifference(viewUsed.completion, completion)
+    }
+
+    func testRemoval() {
+        ZPosition(2).transition(viewUsed, for: .insertion, in: containerUsed)
+
+        let initial = properties
+        XCTAssertNoDifference(viewUsed.initial, initial)
+
+        var animation = properties
+        animation.layer.zPosition = 2
+        XCTAssertNoDifference(viewUsed.animation, animation)
+
+        var completion = properties
+        completion.layer.zPosition = 0
+        XCTAssertNoDifference(viewUsed.completion, completion)
+    }
+}
+
+final class BringToFrontAndSendToBackTests: XCTestCase {
     let uiViewUsed = UIView()
     lazy var viewUsed = AnimatorTransientView(uiViewUsed)
     let properties = AnimatorTransientViewProperties(
