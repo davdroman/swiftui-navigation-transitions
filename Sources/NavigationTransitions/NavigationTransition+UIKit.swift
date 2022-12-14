936df89b4f31fe766d7d0e6051a4e00611372611
David Roman <2538074+davdroman@users.noreply.github.com>
2022-12-14 09:38:30 +0000

    wip

--- Sources/NavigationTransitions/NavigationTransition+UIKit.swift
+++ Sources/NavigationTransitions/NavigationTransition+UIKit.swift
@@ -223,7 +223,7 @@ extension UINavigationController: RuntimeAssociation {
 }
 
 extension UINavigationController {
-    @objc func popToViewController_forceAnimated(_ viewController: UIViewController, animated: Bool) -> [UIViewController]? {
+    @objc fileprivate func popToViewController_forceAnimated(_ viewController: UIViewController, animated: Bool) -> [UIViewController]? {
         popToViewController_forceAnimated(viewController, animated: true)
     }
 }
