3818ceb17706ef08d18f1b8f3623dc8ad475b7e9
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-18 16:00:46 +0100

    document

--- Sources/Animation/Animation.swift
+++ Sources/Animation/Animation.swift
@@ -6,11 +6,21 @@ public struct Animation {
 	@_spi(package) public var duration: Double
 	@_spi(package) public let timingParameters: UITimingCurveProvider
 
+	// Initializes a new `Animation` instance.
+	///
+	/// - Parameters:
+	///   - duration: The duration of the animation.
+	///   - timingParameters: The timing parameters for the animation.
 	public init(duration: Double, timingParameters: UITimingCurveProvider) {
 		self.duration = duration
 		self.timingParameters = timingParameters
 	}
 
+	/// Initializes a new `Animation` instance with a specified curve.
+	///
+	/// - Parameters:
+	///   - duration: The duration of the animation.
+	///   - curve: The curve for the animation.
 	public init(duration: Double, curve: UIView.AnimationCurve) {
 		self.init(duration: duration, timingParameters: UICubicTimingParameters(animationCurve: curve))
 	}
