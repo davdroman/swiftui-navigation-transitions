ff159937bff5f142130ea840521d9935ffe96b89
David Roman <2538074+davdroman@users.noreply.github.com>
2024-07-19 20:05:29 +0100

    WIP

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,83 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+	name: "swiftui-navigation-transitions",
+	platforms: [
+		.iOS(.v13),
+		.macCatalyst(.v13),
+		.tvOS(.v13),
+	],
+    swiftLanguageVersions: [.v6]
+)
+
+// MARK: Dependencies
+
+package.dependencies = [
+	.package(url: "https://github.com/siteline/swiftui-introspect", from: "1.0.0"),
+	.package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"), // dev
+	.package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"), // dev
+]
+
+let SwiftUIIntrospect: Target.Dependency = .product(
+	name: "SwiftUIIntrospect",
+	package: "swiftui-introspect"
+)
+
+let CustomDump: Target.Dependency = .product(
+	name: "CustomDump",
+	package: "swift-custom-dump"
+)
+
+let XCTestDynamicOverlay: Target.Dependency = .product(
+	name: "XCTestDynamicOverlay",
+	package: "xctest-dynamic-overlay"
+)
+
+// MARK: Targets
+
+package.targets += [
+	.target(name: "Animation"),
+
+	.target(name: "Animator"),
+	.testTarget(name: "AnimatorTests", dependencies: [
+		"Animator",
+		"TestUtils",
+	]),
+
+	.target(name: "AtomicTransition", dependencies: [
+		"Animator",
+	]),
+	.testTarget(name: "AtomicTransitionTests", dependencies: [
+		"AtomicTransition",
+		"TestUtils",
+	]),
+
+	.target(name: "NavigationTransition", dependencies: [
+		"Animation",
+		"AtomicTransition",
+	]),
+
+	.target(name: "NavigationTransitions", dependencies: [
+		"NavigationTransition",
+		"RuntimeAssociation",
+		"RuntimeSwizzling",
+		SwiftUIIntrospect,
+	]),
+
+	.target(name: "RuntimeAssociation"),
+	.target(name: "RuntimeSwizzling"),
+
+	.target(name: "TestUtils", dependencies: [
+		CustomDump,
+		"NavigationTransitions",
+		XCTestDynamicOverlay,
+	]),
+]
+
+// MARK: Product
+
+package.products += [
+	.library(name: "NavigationTransitions", targets: ["NavigationTransitions"]),
+]
