c05cc94bd300451217261814bd215ad9fae63d62
David Roman <2538074+davdroman@users.noreply.github.com>
2025-05-09 20:47:30 +0100

    Tidy up Package manifest and migrate to test plan (#146)

--- Package.swift
+++ Package.swift
@@ -8,6 +8,47 @@ let package = Package(
 		.iOS(.v13),
 		.macCatalyst(.v13),
 		.tvOS(.v13),
+	],
+	products: [
+		.library(name: "NavigationTransitions", targets: ["NavigationTransitions"]),
+	],
+	targets: [
+		.target(name: "Animation"),
+
+		.target(name: "Animator"),
+		.testTarget(name: "AnimatorTests", dependencies: [
+			"Animator",
+			"TestUtils",
+		]),
+
+		.target(name: "AtomicTransition", dependencies: [
+			"Animator",
+		]),
+		.testTarget(name: "AtomicTransitionTests", dependencies: [
+			"AtomicTransition",
+			"TestUtils",
+		]),
+
+		.target(name: "NavigationTransition", dependencies: [
+			"Animation",
+			"AtomicTransition",
+		]),
+
+		.target(name: "NavigationTransitions", dependencies: [
+			"NavigationTransition",
+			"RuntimeAssociation",
+			"RuntimeSwizzling",
+			.product(name: "SwiftUIIntrospect", package: "swiftui-introspect"),
+		]),
+
+		.target(name: "RuntimeAssociation"),
+		.target(name: "RuntimeSwizzling"),
+
+		.target(name: "TestUtils", dependencies: [
+			.product(name: "CustomDump", package: "swift-custom-dump"),
+			"NavigationTransitions",
+			.product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
+		]),
 	]
 )
 
@@ -17,66 +58,4 @@ package.dependencies = [
 	.package(url: "https://github.com/siteline/swiftui-introspect", from: "1.0.0"),
 	.package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"), // dev
 	.package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"), // dev
-]
-
-let SwiftUIIntrospect: Target.Dependency = .product(
-	name: "SwiftUIIntrospect",
-	package: "swiftui-introspect"
-)
-
-let CustomDump: Target.Dependency = .product(
-	name: "CustomDump",
-	package: "swift-custom-dump"
-)
-
-let XCTestDynamicOverlay: Target.Dependency = .product(
-	name: "XCTestDynamicOverlay",
-	package: "xctest-dynamic-overlay"
-)
-
-// MARK: Targets
-
-package.targets += [
-	.target(name: "Animation"),
-
-	.target(name: "Animator"),
-	.testTarget(name: "AnimatorTests", dependencies: [
-		"Animator",
-		"TestUtils",
-	]),
-
-	.target(name: "AtomicTransition", dependencies: [
-		"Animator",
-	]),
-	.testTarget(name: "AtomicTransitionTests", dependencies: [
-		"AtomicTransition",
-		"TestUtils",
-	]),
-
-	.target(name: "NavigationTransition", dependencies: [
-		"Animation",
-		"AtomicTransition",
-	]),
-
-	.target(name: "NavigationTransitions", dependencies: [
-		"NavigationTransition",
-		"RuntimeAssociation",
-		"RuntimeSwizzling",
-		SwiftUIIntrospect,
-	]),
-
-	.target(name: "RuntimeAssociation"),
-	.target(name: "RuntimeSwizzling"),
-
-	.target(name: "TestUtils", dependencies: [
-		CustomDump,
-		"NavigationTransitions",
-		XCTestDynamicOverlay,
-	]),
-]
-
-// MARK: Product
-
-package.products += [
-	.library(name: "NavigationTransitions", targets: ["NavigationTransitions"]),
 ]
