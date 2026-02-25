4ee85350f5005ded1a2d825cc4a158f3325bdad4
David Roman <2538074+davdroman@users.noreply.github.com>
2026-02-25 21:42:36 +0000

    Bump to Swift 6.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 
 import PackageDescription
 
@@ -73,5 +73,6 @@ for target in package.targets {
 	target.swiftSettings? += [
 		.enableUpcomingFeature("ExistentialAny"),
 		.enableUpcomingFeature("InternalImportsByDefault"),
+		.enableUpcomingFeature("MemberImportVisibility"),
 	]
 }
